19d8d67643156ebfa2cb9e2b3e8683098927f833
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-08-01 11:08:47 -0300

    Corrigindo type do campo id na model de user

--- imc-tracker-backend/src/models/userModel.ts
+++ imc-tracker-backend/src/models/userModel.ts
@@ -5,7 +5,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity("usuario")
 export class User {
   @PrimaryGeneratedColumn("uuid")
-  id: number;
+  id: string;
 
   @Column({ length: 60, nullable: false })
   nome: string;
