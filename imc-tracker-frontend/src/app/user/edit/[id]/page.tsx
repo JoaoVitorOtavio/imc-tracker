e2103d474e995055fe9119e00e4b2f356359927f
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-08-03 17:45:53 -0300

    Pagina de edit user

---
+++ imc-tracker-frontend/src/app/user/edit/[id]/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import UserForm from "@/components/UserForm";
+import { useGetUser } from "@/hooks/user/useGetUser";
+import axios from "axios";
+import { useParams, notFound } from "next/navigation";
+import { useEffect } from "react";
+
+export default function CreateUser() {
+  const params = useParams();
+  const userId = params.id;
+
+  const {
+    data: user,
+    isError,
+    isLoading,
+    error,
+  } = useGetUser(userId as string);
+
+  useEffect(() => {
+    if (error && axios.isAxiosError(error)) {
+      const status = error.response?.status;
+
+      if (status === 404 || status === 400) {
+        notFound();
+      }
+    }
+  }, [error, userId]);
+
+  if (isLoading) {
+    return <p>Carregando usuário...</p>;
+  }
+
+  if (isError || !user) {
+    return <p>Erro ao carregar usuário.</p>;
+  }
+
+  return (
+    <div>
+      <UserForm isEdit user={user} />
+    </div>
+  );
+}
