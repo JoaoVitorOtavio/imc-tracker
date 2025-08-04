ba9e781419879a908597c33eb54b5edaee54cbf0
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-08-04 15:07:18 -0300

    Corrigindo problema da funcao para calcular imc

--- imc-tracker-backend/src/common/utils/calculateBmi.ts
+++ imc-tracker-backend/src/common/utils/calculateBmi.ts
@@ -1,13 +1,13 @@
 export function calculateBmi(imc: number): string {
   if (imc < 18.5) {
     return "Abaixo do peso";
-  } else if (imc >= 18.5 && imc <= 24.9) {
+  } else if (imc >= 18.5 && imc <= 24.99) {
     return "Peso normal";
-  } else if (imc >= 25 && imc <= 29.9) {
+  } else if (imc >= 25 && imc <= 29.99) {
     return "Sobrepeso";
-  } else if (imc >= 30 && imc <= 34.9) {
+  } else if (imc >= 30 && imc <= 34.99) {
     return "Obesidade grau I";
-  } else if (imc >= 35 && imc <= 39.9) {
+  } else if (imc >= 35 && imc <= 39.99) {
     return "Obesidade grau II";
   } else if (imc >= 40) {
     return "Obesidade grau III";
