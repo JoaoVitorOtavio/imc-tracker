12a692a9c32beb1fb108cf710b6d1730c5300bfd
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-08-05 22:53:49 -0300

    Arrumando design da pagina de listagem de usuario

--- imc-tracker-frontend/src/app/user/list/page.tsx
+++ imc-tracker-frontend/src/app/user/list/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Footer from "@/components/Footer";
+import Header from "@/components/Header/Header";
 import Modal from "@/components/Modal";
 import { useDeleteUser } from "@/hooks/user/useDeleteUser";
 import { useGetUsers } from "@/hooks/user/useGetUsers";
@@ -8,7 +10,6 @@ import {
   Button,
   ButtonGroup,
   Center,
-  Field,
   Flex,
   IconButton,
   Input,
@@ -19,10 +20,12 @@ import {
   Table,
   VStack,
   Text,
+  Heading,
 } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
+import { FaCirclePlus } from "react-icons/fa6";
 
 export default function UserList() {
   const router = useRouter();
@@ -70,107 +73,136 @@ export default function UserList() {
     );
 
   return (
-    <Stack width="full" gap="5">
-      {!users?.data.length ? (
-        <div>Nenhum usuário encontrado</div>
-      ) : (
-        <>
-          <Skeleton loading={loadingUsers || isDeleteLoading}>
-            <Flex gap="4" align="center">
-              <Field.Root>
+    <Flex minHeight={"100vh"} direction={"column"} justify={"space-between"}>
+      <Header />
+      <Stack width="full" gap="5" px={12} mt={5}>
+        {!users?.data.length ? (
+          <div>Nenhum usuário encontrado</div>
+        ) : (
+          <>
+            <Heading size="xl" display={"flex"} justifyContent={"center"}>
+              Listagem de avaliações de usuários
+            </Heading>
+            <Skeleton loading={loadingUsers || isDeleteLoading}>
+              <Flex
+                gap="4"
+                align="center"
+                mt={5}
+                px={5}
+                direction={{ base: "column", md: "row" }}
+              >
+                <Button
+                  width={{ base: "80%", md: "auto" }}
+                  colorPalette="green"
+                  variant="surface"
+                  size="sm"
+                  onClick={() => router.push("/user/create")}
+                >
+                  <FaCirclePlus /> Adicionar novo usuário
+                </Button>
                 <Input
+                  width="80%"
                   onChange={(e) => handleSearch(e.target.value)}
-                  id="saerchInput"
+                  id="searchInput"
                   size="sm"
                   placeholder="Pesquisar por nome ou usuário"
+                  bg="white"
+                  color="black"
+                  _placeholder={{ color: "gray.500" }}
+                  borderColor="gray.300"
                 />
-              </Field.Root>
-            </Flex>
-          </Skeleton>
-          <Skeleton loading={loadingUsers || isDeleteLoading}>
-            <Table.Root size="md" variant="outline" striped>
-              <Table.Header>
-                <Table.Row>
-                  <Table.ColumnHeader>Nome</Table.ColumnHeader>
-                  <Table.ColumnHeader>Usuario</Table.ColumnHeader>
-                  <Table.ColumnHeader>Perfil</Table.ColumnHeader>
-                  <Table.ColumnHeader>Situação</Table.ColumnHeader>
-                  <Table.ColumnHeader>Data de inclusão</Table.ColumnHeader>
-                  <Table.ColumnHeader textAlign="center"></Table.ColumnHeader>
-                  <Table.ColumnHeader></Table.ColumnHeader>
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {users?.data.map((user) => (
-                  <Table.Row key={user.id}>
-                    <Table.Cell>{user.nome}</Table.Cell>
-                    <Table.Cell>{user.usuario}</Table.Cell>
-                    <Table.Cell>{user.perfil}</Table.Cell>
-                    <Table.Cell
-                      color={user.situacao === "ativo" ? "green" : "red"}
-                    >
-                      {user.situacao}
-                    </Table.Cell>
-                    <Table.Cell>{user.dt_inclusao.toString()}</Table.Cell>
-                    <Table.Cell>
-                      <Button
-                        colorPalette="blue"
-                        variant="outline"
-                        size="sm"
-                        onClick={() => router.push(`/user/edit/${user.id}`)}
-                      >
-                        Editar
-                      </Button>
-                    </Table.Cell>
-                    <Table.Cell>
-                      <Modal
-                        confirmFunc={() => onDelete(user.id)}
-                        btnTitle="Excluir"
-                        cancelBtnTitle="Cancelar"
-                        confirmBtnTitle="Excluir"
-                        btnColorPalette="red"
-                        modalTitle="Tem certeza que deseja excluir?"
-                        modalDescription="Esta ação é irreversível"
-                      />
-                    </Table.Cell>
-                  </Table.Row>
-                ))}
-              </Table.Body>
-            </Table.Root>
+              </Flex>
+            </Skeleton>
+            <Skeleton loading={loadingUsers || isDeleteLoading}>
+              <div style={{ overflowX: "auto" }}>
+                <Table.Root size="md" variant="outline" striped>
+                  <Table.Header>
+                    <Table.Row>
+                      <Table.ColumnHeader>Nome</Table.ColumnHeader>
+                      <Table.ColumnHeader>Usuario</Table.ColumnHeader>
+                      <Table.ColumnHeader>Perfil</Table.ColumnHeader>
+                      <Table.ColumnHeader>Situação</Table.ColumnHeader>
+                      <Table.ColumnHeader>Data de inclusão</Table.ColumnHeader>
+                      <Table.ColumnHeader textAlign="center"></Table.ColumnHeader>
+                      <Table.ColumnHeader></Table.ColumnHeader>
+                    </Table.Row>
+                  </Table.Header>
+                  <Table.Body>
+                    {users?.data.map((user) => (
+                      <Table.Row key={user.id}>
+                        <Table.Cell>{user.nome}</Table.Cell>
+                        <Table.Cell>{user.usuario}</Table.Cell>
+                        <Table.Cell>{user.perfil}</Table.Cell>
+                        <Table.Cell
+                          color={user.situacao === "ativo" ? "green" : "red"}
+                        >
+                          {user.situacao}
+                        </Table.Cell>
+                        <Table.Cell>{user.dt_inclusao.toString()}</Table.Cell>
+                        <Table.Cell>
+                          <Button
+                            colorPalette="blue"
+                            variant="outline"
+                            size="sm"
+                            onClick={() => router.push(`/user/edit/${user.id}`)}
+                          >
+                            Editar
+                          </Button>
+                        </Table.Cell>
+                        <Table.Cell>
+                          <Modal
+                            confirmFunc={() => onDelete(user.id)}
+                            btnTitle="Excluir"
+                            cancelBtnTitle="Cancelar"
+                            confirmBtnTitle="Excluir"
+                            btnColorPalette="red"
+                            modalTitle="Tem certeza que deseja excluir?"
+                            modalDescription="Esta ação é irreversível"
+                          />
+                        </Table.Cell>
+                      </Table.Row>
+                    ))}
+                  </Table.Body>
+                </Table.Root>
 
-            <Pagination.Root
-              count={users?.total}
-              pageSize={LIMIT}
-              page={page}
-              onPageChange={(e) => setPage(e.page)}
-            >
-              <ButtonGroup variant="ghost" size="sm" wrap="wrap">
-                <Pagination.PrevTrigger asChild>
-                  <IconButton>
-                    <LuChevronLeft />
-                  </IconButton>
-                </Pagination.PrevTrigger>
+                <Pagination.Root
+                  count={users?.total}
+                  pageSize={LIMIT}
+                  page={page}
+                  onPageChange={(e) => setPage(e.page)}
+                  mb={10}
+                  colorPalette={"blue"}
+                >
+                  <ButtonGroup variant="ghost" size="sm" wrap="wrap">
+                    <Pagination.PrevTrigger asChild>
+                      <IconButton>
+                        <LuChevronLeft />
+                      </IconButton>
+                    </Pagination.PrevTrigger>
 
-                <Pagination.Items
-                  render={(page) => (
-                    <IconButton
-                      variant={{ base: "ghost", _selected: "outline" }}
-                    >
-                      {page.value}
-                    </IconButton>
-                  )}
-                />
+                    <Pagination.Items
+                      render={(page) => (
+                        <IconButton
+                          variant={{ base: "ghost", _selected: "outline" }}
+                        >
+                          {page.value}
+                        </IconButton>
+                      )}
+                    />
 
-                <Pagination.NextTrigger asChild>
-                  <IconButton>
-                    <LuChevronRight />
-                  </IconButton>
-                </Pagination.NextTrigger>
-              </ButtonGroup>
-            </Pagination.Root>
-          </Skeleton>
-        </>
-      )}
-    </Stack>
+                    <Pagination.NextTrigger asChild>
+                      <IconButton>
+                        <LuChevronRight />
+                      </IconButton>
+                    </Pagination.NextTrigger>
+                  </ButtonGroup>
+                </Pagination.Root>
+              </div>
+            </Skeleton>
+          </>
+        )}
+      </Stack>
+      <Footer />
+    </Flex>
   );
 }
