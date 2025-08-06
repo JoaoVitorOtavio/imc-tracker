66d59629ad2c0ed3cb810b13516af60c88555ab9
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-08-05 22:54:41 -0300

    Arrumando design da pagina de listagem de avaliacoes de imc

--- imc-tracker-frontend/src/app/bmi-evaluation/list/page.tsx
+++ imc-tracker-frontend/src/app/bmi-evaluation/list/page.tsx
@@ -1,26 +1,34 @@
 "use client";
 
 import { Perfil } from "@/common/enums/perfil.enum";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header/Header";
 import Modal from "@/components/Modal";
 import { useDeleteBmiEvaluation } from "@/hooks/bmiEvaluation/useDeleteBmiEvaluation";
 import { useGetBmiEvaluations } from "@/hooks/bmiEvaluation/useGetBmiEvaluations";
 import { useGetUsers } from "@/hooks/user/useGetUsers";
 import {
+  Box,
   Button,
   ButtonGroup,
+  Center,
   Field,
+  Flex,
   Heading,
   IconButton,
   NativeSelect,
   Pagination,
   Skeleton,
+  Spinner,
   Stack,
   Table,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { notFound, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FaCirclePlus } from "react-icons/fa6";
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 
 export default function ListBmiEvaluation() {
@@ -68,140 +76,216 @@ export default function ListBmiEvaluation() {
     deleteBmiEvaluation(id);
   };
 
+  if (loadingBmiEvaluations)
+    return (
+      <Box
+        position="fixed"
+        inset={0}
+        bg="whiteAlpha.800"
+        zIndex={1000}
+        backdropFilter="blur(6px)"
+      >
+        <Center h="100vh">
+          <VStack gap={4}>
+            <Spinner color="teal.500" size="xl" />
+            <Text fontSize="lg" color="gray.600">
+              Carregando informações...
+            </Text>
+          </VStack>
+        </Center>
+      </Box>
+    );
+
   return (
-    <div>
-      <Stack width="full" gap="5">
-        <Heading size="xl">Products</Heading>
-        <Text>Filtrar por aluno e/ou professor </Text>
-        <Skeleton loading={loadingTeachers}>
-          <Field.Root>
-            <Field.Label>Professor</Field.Label>
-            <NativeSelect.Root
-              onChange={(e) =>
-                setTeacherId((e.target as HTMLSelectElement).value)
-              }
-            >
-              <NativeSelect.Field>
-                <option key="default" value="">
-                  Todos os professores
-                </option>
-                {teachers?.data.map((professor) => (
-                  <option key={professor.id} value={professor.id}>
-                    {professor.nome}
-                  </option>
-                ))}
-              </NativeSelect.Field>
-              <NativeSelect.Indicator />
-            </NativeSelect.Root>
-          </Field.Root>
-        </Skeleton>
-        <Skeleton loading={loadingStudents}>
-          <Field.Root>
-            <Field.Label>Aluno</Field.Label>
-            <NativeSelect.Root
-              onChange={(e) =>
-                setStudentId((e.target as HTMLSelectElement).value)
-              }
-            >
-              <NativeSelect.Field>
-                <option key="default" value="">
-                  Todos os alunos
-                </option>
-                {students?.data.map((aluno) => (
-                  <option key={aluno.id} value={aluno.id}>
-                    {aluno.nome}
-                  </option>
-                ))}
-              </NativeSelect.Field>
-              <NativeSelect.Indicator />
-            </NativeSelect.Root>
-          </Field.Root>
-        </Skeleton>
-        <Skeleton loading={loadingBmiEvaluations || isDeleteLoading}>
-          <Table.Root size="md" variant="outline" striped>
-            <Table.Header>
-              <Table.Row>
-                <Table.ColumnHeader>Professor</Table.ColumnHeader>
-                <Table.ColumnHeader>Aluno</Table.ColumnHeader>
-                <Table.ColumnHeader>Peso</Table.ColumnHeader>
-                <Table.ColumnHeader>Altura</Table.ColumnHeader>
-                <Table.ColumnHeader>IMC</Table.ColumnHeader>
-                <Table.ColumnHeader>Classificação</Table.ColumnHeader>
-                <Table.ColumnHeader>Data da avaliação</Table.ColumnHeader>
-                <Table.ColumnHeader textAlign="center"></Table.ColumnHeader>
-                <Table.ColumnHeader></Table.ColumnHeader>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {bmiEvaluations?.data.map((bmiEvaluation) => (
-                <Table.Row key={bmiEvaluation.id}>
-                  <Table.Cell>{bmiEvaluation.usuarioAvaliador.nome}</Table.Cell>
-                  <Table.Cell>{bmiEvaluation.usuarioAvaliado.nome}</Table.Cell>
-                  <Table.Cell>{bmiEvaluation.peso}</Table.Cell>
-                  <Table.Cell>{bmiEvaluation.altura}</Table.Cell>
-                  <Table.Cell>{bmiEvaluation.imc}</Table.Cell>
-                  <Table.Cell>{bmiEvaluation.classificacao}</Table.Cell>
-                  <Table.Cell>
-                    {bmiEvaluation.dt_inclusao.toString()}
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Button
-                      colorPalette="blue"
-                      variant="outline"
-                      size="sm"
-                      onClick={() =>
-                        router.push(`/bmi-evaluation/edit/${bmiEvaluation.id}`)
-                      }
-                    >
-                      Editar
-                    </Button>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Modal
-                      confirmFunc={() => onDelete(bmiEvaluation.id)}
-                      btnTitle="Excluir"
-                      cancelBtnTitle="Cancelar"
-                      confirmBtnTitle="Excluir"
-                      btnColorPalette="red"
-                      modalTitle="Tem certeza que deseja excluir?"
-                      modalDescription="Esta ação é irreversível"
-                    />
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </Table.Root>
+    <Flex minHeight={"100vh"} direction={"column"} justify={"space-between"}>
+      <Header />
+      <Stack width="full" gap="5" mt={5} px={12}>
+        <Heading size="xl" display={"flex"} justifyContent={"center"}>
+          Listagem de avaliações de IMC
+        </Heading>
+        <Flex direction="column" alignItems="center" mt={5} w="100%">
+          <Text fontWeight="bold" mb={4}>
+            Filtrar por aluno e/ou professor
+          </Text>
 
-          <Pagination.Root
-            count={bmiEvaluations?.total}
-            pageSize={LIMIT}
-            page={page}
-            onPageChange={(e) => setPage(e.page)}
+          <Flex
+            w="100%"
+            gap={4}
+            flexWrap="wrap"
+            justify="center"
+            direction={{ base: "column", md: "row" }}
+            align="center"
           >
-            <ButtonGroup variant="ghost" size="sm" wrap="wrap">
-              <Pagination.PrevTrigger asChild>
-                <IconButton>
-                  <LuChevronLeft />
-                </IconButton>
-              </Pagination.PrevTrigger>
+            <Skeleton loading={loadingTeachers} w={{ base: "80%", md: "30%" }}>
+              <Field.Root w="100%">
+                <Field.Label>Professor</Field.Label>
+                <NativeSelect.Root
+                  onChange={(e) =>
+                    setTeacherId((e.target as HTMLSelectElement).value)
+                  }
+                >
+                  <NativeSelect.Field
+                    bg="white"
+                    color="black"
+                    _placeholder={{ color: "gray.500" }}
+                    borderColor="gray.300"
+                    w="100%"
+                  >
+                    <option key="default" value="">
+                      Todos os professores
+                    </option>
+                    {teachers?.data.map((professor) => (
+                      <option key={professor.id} value={professor.id}>
+                        {professor.nome}
+                      </option>
+                    ))}
+                  </NativeSelect.Field>
+                  <NativeSelect.Indicator />
+                </NativeSelect.Root>
+              </Field.Root>
+            </Skeleton>
 
-              <Pagination.Items
-                render={(page) => (
-                  <IconButton variant={{ base: "ghost", _selected: "outline" }}>
-                    {page.value}
+            <Skeleton loading={loadingStudents} w={{ base: "80%", md: "30%" }}>
+              <Field.Root w="100%">
+                <Field.Label>Aluno</Field.Label>
+                <NativeSelect.Root
+                  onChange={(e) =>
+                    setStudentId((e.target as HTMLSelectElement).value)
+                  }
+                >
+                  <NativeSelect.Field
+                    bg="white"
+                    color="black"
+                    _placeholder={{ color: "gray.500" }}
+                    borderColor="gray.300"
+                    w="100%"
+                  >
+                    <option key="default" value="">
+                      Todos os alunos
+                    </option>
+                    {students?.data.map((aluno) => (
+                      <option key={aluno.id} value={aluno.id}>
+                        {aluno.nome}
+                      </option>
+                    ))}
+                  </NativeSelect.Field>
+                  <NativeSelect.Indicator />
+                </NativeSelect.Root>
+              </Field.Root>
+            </Skeleton>
+
+            <Flex width={"100%"} justifyContent={"center"}>
+              <Button
+                colorPalette="green"
+                variant="surface"
+                w={{ base: "80%", md: "30%" }}
+                alignSelf="flex-end"
+                onClick={() => router.push("/bmi-evaluation/create")}
+              >
+                <FaCirclePlus style={{ marginRight: "8px" }} />
+                Adicionar nova avaliação
+              </Button>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Skeleton loading={loadingBmiEvaluations || isDeleteLoading}>
+          <div style={{ overflowX: "auto" }}>
+            <Table.Root size="md" variant="outline" striped>
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeader>Professor</Table.ColumnHeader>
+                  <Table.ColumnHeader>Aluno</Table.ColumnHeader>
+                  <Table.ColumnHeader>Peso</Table.ColumnHeader>
+                  <Table.ColumnHeader>Altura</Table.ColumnHeader>
+                  <Table.ColumnHeader>IMC</Table.ColumnHeader>
+                  <Table.ColumnHeader>Classificação</Table.ColumnHeader>
+                  <Table.ColumnHeader>Data da avaliação</Table.ColumnHeader>
+                  <Table.ColumnHeader textAlign="center"></Table.ColumnHeader>
+                  <Table.ColumnHeader></Table.ColumnHeader>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {bmiEvaluations?.data.map((bmiEvaluation) => (
+                  <Table.Row key={bmiEvaluation.id}>
+                    <Table.Cell>
+                      {bmiEvaluation.usuarioAvaliador.nome}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {bmiEvaluation.usuarioAvaliado.nome}
+                    </Table.Cell>
+                    <Table.Cell>{bmiEvaluation.peso}</Table.Cell>
+                    <Table.Cell>{bmiEvaluation.altura}</Table.Cell>
+                    <Table.Cell>{bmiEvaluation.imc}</Table.Cell>
+                    <Table.Cell>{bmiEvaluation.classificacao}</Table.Cell>
+                    <Table.Cell>
+                      {bmiEvaluation.dt_inclusao.toString()}
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Button
+                        colorPalette="blue"
+                        variant="outline"
+                        size="sm"
+                        onClick={() =>
+                          router.push(
+                            `/bmi-evaluation/edit/${bmiEvaluation.id}`
+                          )
+                        }
+                      >
+                        Editar
+                      </Button>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Modal
+                        confirmFunc={() => onDelete(bmiEvaluation.id)}
+                        btnTitle="Excluir"
+                        cancelBtnTitle="Cancelar"
+                        confirmBtnTitle="Excluir"
+                        btnColorPalette="red"
+                        modalTitle="Tem certeza que deseja excluir?"
+                        modalDescription="Esta ação é irreversível"
+                      />
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table.Root>
+
+            <Pagination.Root
+              count={bmiEvaluations?.total}
+              pageSize={LIMIT}
+              page={page}
+              onPageChange={(e) => setPage(e.page)}
+              mb={10}
+              colorPalette={"blue"}
+            >
+              <ButtonGroup variant="ghost" size="sm" wrap="wrap">
+                <Pagination.PrevTrigger asChild>
+                  <IconButton>
+                    <LuChevronLeft />
                   </IconButton>
-                )}
-              />
+                </Pagination.PrevTrigger>
 
-              <Pagination.NextTrigger asChild>
-                <IconButton>
-                  <LuChevronRight />
-                </IconButton>
-              </Pagination.NextTrigger>
-            </ButtonGroup>
-          </Pagination.Root>
+                <Pagination.Items
+                  render={(page) => (
+                    <IconButton
+                      variant={{ base: "ghost", _selected: "outline" }}
+                    >
+                      {page.value}
+                    </IconButton>
+                  )}
+                />
+
+                <Pagination.NextTrigger asChild>
+                  <IconButton>
+                    <LuChevronRight />
+                  </IconButton>
+                </Pagination.NextTrigger>
+              </ButtonGroup>
+            </Pagination.Root>
+          </div>
         </Skeleton>
       </Stack>
-    </div>
+      <Footer />
+    </Flex>
   );
 }
